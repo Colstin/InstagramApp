43eef8e859ee1de6ae27c7b87aa86b1cd4718c81
Colstin Donaldson <colstin50@gmail.com>
2023-07-07 10:53:27 -0700

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Colstin Donaldson on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
